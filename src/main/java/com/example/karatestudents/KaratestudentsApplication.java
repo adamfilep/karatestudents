b3033e57d1fcdcd362d9f74b7e2899de4d554b69
adamfilep <adamfilep@gmail.com>
2023-03-27 16:39:13 +0200

    Initial commit

---
+++ src/main/java/com/example/karatestudents/KaratestudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.karatestudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaratestudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaratestudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/karatestudents/KaratestudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karatestudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaratestudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
