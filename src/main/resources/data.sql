f3831f87c5c83c1f02482fb3298921345449abbb
adamfilep <adamfilep@gmail.com>
2023-04-07 16:15:26 +0200

    Add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+insert into student (date_of_birth, is_student, name, std_rank, started_karate)
+values (
+        '1998-04-05',
+        true,
+        'Bruce Lee',
+        'WHITE_BELT',
+        '2010-10-05'
+        );
+
+insert into student (date_of_birth, is_student, name, std_rank, started_karate)
+values (
+        '1960-11-21',
+        true,
+        'IP Man',
+        'BLUE_BELT',
+        '1990-05-20'
+        );
+
+insert into student (date_of_birth, is_student, name, std_rank, started_karate)
+values (
+        '2010-12-11',
+        false,
+        'Scott Adkins',
+        'FIRST_DAN',
+        '2017-02-01'
+        );
+
