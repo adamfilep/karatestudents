87a300631a117dfb47103ce13c372fdb3b4e690f
adamfilep <adamfilep@gmail.com>
2023-04-09 20:38:56 +0200

    Add lombok annotations to TrainerDto

--- src/main/java/com/example/karatestudents/model/dto/TrainerDto.java
+++ src/main/java/com/example/karatestudents/model/dto/TrainerDto.java
@@ -4,7 +4,13 @@ import com.example.karatestudents.model.enums.Rank;
 import com.example.karatestudents.validation.ValueOfEnum;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
+@Builder
+@AllArgsConstructor
+@Data
 public class TrainerDto {
 
     @NotBlank(message = "Name cannot be blank")
