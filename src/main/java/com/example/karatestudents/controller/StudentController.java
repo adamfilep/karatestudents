ec0b60e003d14e161c7deffd752d8bae4f9e0f05
adamfilep <adamfilep@gmail.com>
2023-04-11 03:11:23 +0200

    Add error handling to StudentController

--- src/main/java/com/example/karatestudents/controller/StudentController.java
+++ src/main/java/com/example/karatestudents/controller/StudentController.java
@@ -7,8 +7,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,46 +30,69 @@ public class StudentController {
 
     @Operation(summary = "Save new student")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "New student saved")
+            @ApiResponse(responseCode = "201", description = "New student saved"),
+            @ApiResponse(responseCode = "400", description = "Request JSON was not correct")
     })
     @PostMapping
-    public void saveStudent(@Valid @RequestBody StudentDto studentDto) {
-        studentService.saveStudent(studentDto);
+    public ResponseEntity<Student> saveStudent(@Valid @RequestBody StudentDto studentDto) {
+        try {
+            studentService.saveStudent(studentDto);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Operation(summary = "Get all students")
     @ApiResponses(value = {
-           @ApiResponse(responseCode = "200", description = "Students found")
+            @ApiResponse(responseCode = "200", description = "Students found")
     })
     @GetMapping
-    public List<Student> getAllStudents() {
-        return studentService.getAllStudents();
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return ResponseEntity.ok(studentService.getAllStudents());
     }
 
     @Operation(summary = "Get student with a specific id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Student found")
+            @ApiResponse(responseCode = "200", description = "Student found"),
+            @ApiResponse(responseCode = "404", description = "Student not found")
     })
     @GetMapping("/{id}")
-    public Student getStudentById(@PathVariable("id") Long id) {
-        return studentService.getStudentById(id);
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(studentService.getStudentById(id));
+        } catch (EntityNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Operation(summary = "Update student with a specific id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Student updated")
+            @ApiResponse(responseCode = "200", description = "Student updated"),
+            @ApiResponse(responseCode = "400", description = "Request JSON or id was not correct")
     })
     @PutMapping("/{id}")
-    public void updateStudent(@PathVariable("id") Long id, @Valid @RequestBody StudentDto updatedStudentDto) {
-        studentService.updateStudent(id, updatedStudentDto);
+    public ResponseEntity<Student> updateStudent(@PathVariable("id") Long id, @Valid @RequestBody StudentDto updatedStudentDto) {
+        try {
+            studentService.updateStudent(id, updatedStudentDto);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Delete student with a specific id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Student deleted")
+            @ApiResponse(responseCode = "204", description = "Student deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Student to be deleted not found"),
     })
     @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable("id") Long id) {
-        studentService.deleteStudentById(id);
+    public ResponseEntity<Student> deleteStudent(@PathVariable("id") Long id) {
+        try {
+            studentService.deleteStudentById(id);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 }
